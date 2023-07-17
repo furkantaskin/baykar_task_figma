bdf0ec905f33a88d266c0e8efd09d99a1b61524e
Furkan Taşkın <furkan.taskin@tuta.io>
2023-07-17 17:43:33 +0300

    Update header menu function for js

--- src/lib/common.js
+++ src/lib/common.js
@@ -1,19 +1,28 @@
 export function mobileMenu() {
+
+  // Open mobile menu on button click for mobile
   let showMenu = false;
   const mobileMenu = document.querySelector('.mobileMenu');
+  const menuSvg = document.querySelector('.menuButton svg');
   function toggleMenu(isActive) {
+
+    // Add/remove class to elements and update of SVG to display menu is opened/closed
     if (isActive) {
       document
-        .querySelectorAll('body, .mobileMenu, .menuButton')
+        .querySelectorAll('body, .mobileMenu, .menuButton, .sm_nav')
         .forEach((e) => {
           e.classList.add('show');
         });
+      menuSvg.innerHTML =
+        '<path d="M3 5h18M3 12h18M3 19h18" stroke="#000" stroke-linecap="round" stroke-linejoin="round"/>';
     } else {
       document
-        .querySelectorAll('body, .mobileMenu, .menuButton')
+        .querySelectorAll('body, .mobileMenu, .menuButton, .sm_nav')
         .forEach((e) => {
           e.classList.remove('show');
         });
+      menuSvg.innerHTML = `
+      <path d="M3 5h8m-8 7h13M3 19h18" stroke="#000" stroke-linecap="round" stroke-linejoin="round"/>`;
     }
   }
 
@@ -23,10 +32,4 @@ export function mobileMenu() {
       showMenu = !showMenu;
       toggleMenu(showMenu);
     });
-
-  mobileMenu.innerHTML =
-    document.querySelector('nav.navbar').innerHTML;
-
-  mobileMenu.style.paddingTop =
-    document.querySelector('.mobileHeader').clientHeight * 1.5 + 'px';
 }
