1e2c5682f0dc1df35a5edf616ba473b6751f9afb
Furkan Taşkın <furkan.taskin@tuta.io>
2023-07-17 17:41:06 +0300

    Create & style header area

--- theme/header.php
+++ theme/header.php
@@ -25,26 +25,59 @@ include "components/svg-sprite.php";
 
 <body>
 
-    <header></header>
-
-    <div class="mobileHeader">
+    <header class="header_lg">
         <div class="container">
-            <div class="row">
-                <div class="col-6">
-                    <a href="" class="logo">
-                        <img src="<?= domain ?>assets/img/svg/logo.svg" alt="">
-                    </a>
-                </div>
-                <div class="col-6">
-                    <div class="menuButton d-flex a-c">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                </div>
+            <a href="index.php" class="header_logo" title="Collers Sportswear">Collers</a>
+            <nav role="navigation" aria-label="Main menu">
+                <ul>
+                    <li>
+                        <a href="" title="Products">Products</a>
+                    </li>
+                    <li>
+                        <a href="" title="Solutions">Solutions</a>
+                    </li>
+                    <li>
+                        <a href="" title="Pricing">Pricing</a>
+                    </li>
+                    <li>
+                        <a href="" title="Resources">Resources</a>
+                    </li>
+                    <li>
+                        <a href="" title="Log In">Log In</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <header class="header_sm p-rel">
+        <div class="container d-flex j-b a-c">
+            <a href="index.php" title="Collers Sportswear" class="header_logo">Collers</a>
+            <div class="menuButton d-flex f-c a-e">
+                <svg width="24" height="24" stroke-width="1.5" fill="none" xmlns="http://www.w3.org/2000/svg"
+                    color="#000">
+                    <path d="M3 5h8m-8 7h13M3 19h18" stroke="#000" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
             </div>
         </div>
-    </div>
-
-    <div class="mobileMenu"></div>
+        <nav role="navigation" class="sm_nav" aria-label="Main menu">
+            <ul class="d-flex f-c a-s">
+                <li>
+                    <a href="" title="Products">Products</a>
+                </li>
+                <li>
+                    <a href="" title="Solutions">Solutions</a>
+                </li>
+                <li>
+                    <a href="" title="Pricing">Pricing</a>
+                </li>
+                <li>
+                    <a href="" title="Resources">Resources</a>
+                </li>
+                <li>
+                    <a href="" title="Log In">Log In</a>
+                </li>
+            </ul>
+            <a href="" title="Sign up now" class="btn-primary">Sign up now</a>
+        </nav>
+    </header>
