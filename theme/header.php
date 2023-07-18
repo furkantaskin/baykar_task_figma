7eaf6085b58c1db360fcba4db0f9071dd2bd270c
Furkan Taşkın <furkan.taskin@tuta.io>
2023-07-18 03:53:19 +0300

    Format file

--- theme/header.php
+++ theme/header.php
@@ -25,41 +25,36 @@ include "components/svg-sprite.php";
 <body>
 
     <header class="header_lg">
-        <div class="container">
-            <a href="index.php" class="header_logo" title="Collers Sportswear">Collers</a>
-            <nav role="navigation" aria-label="Main menu">
-                <ul>
-                    <li>
-                        <a href="" title="Products">Products</a>
-                    </li>
-                    <li>
-                        <a href="" title="Solutions">Solutions</a>
-                    </li>
-                    <li>
-                        <a href="" title="Pricing">Pricing</a>
-                    </li>
-                    <li>
-                        <a href="" title="Resources">Resources</a>
-                    </li>
-                    <li>
-                        <a href="" title="Log In">Log In</a>
-                    </li>
-                </ul>
-                <a href="" title="Sign up now" class="btn btn-primary">Sign up now</a>
+        <a href="index.php" class="header_logo" title="Collers Sportswear">Collers</a>
+        <nav role="navigation" aria-label="Main menu">
+            <ul>
+                <li>
+                    <a href="" title="Products">Products</a>
+                </li>
+                <li>
+                    <a href="" title="Solutions">Solutions</a>
+                </li>
+                <li>
+                    <a href="" title="Pricing">Pricing</a>
+                </li>
+                <li>
+                    <a href="" title="Resources">Resources</a>
+                </li>
+                <li>
+                    <a href="" title="Log In">Log In</a>
+                </li>
+            </ul>
+            <a href="" title="Sign up now" class="btn btn-primary">Sign up now</a>
 
-            </nav>
-        </div>
+        </nav>
     </header>
 
-    <header class="header_sm p-rel">
-        <div class="container d-flex j-b a-c">
-            <a href="index.php" title="Collers Sportswear" class="header_logo">Collers</a>
-            <div class="menuButton d-flex f-c a-e">
-                <svg width="24" height="24" stroke-width="1.5" fill="none" xmlns="http://www.w3.org/2000/svg"
-                    color="#000">
-                    <path d="M3 5h8m-8 7h13M3 19h18" stroke="#000" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-            </div>
+    <header class="header_sm p-rel d-flex j-b a-c">
+        <a href="index.php" title="Collers Sportswear" class="header_logo">Collers</a>
+        <div class="menuButton d-flex f-c a-e">
+            <svg width="24" height="24" stroke-width="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000">
+                <path d="M3 5h8m-8 7h13M3 19h18" stroke="#000" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
         </div>
         <nav role="navigation" class="sm_nav" aria-label="Main menu">
             <ul class="d-flex f-c a-s">
